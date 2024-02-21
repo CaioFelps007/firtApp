e40002c90f65c504f103a845fa6b9426675c2acd
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2024-02-21 15:32:13 -0300

    att1

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
       </View>
       <View style={styles.secondaryContainer}>
 
-{/*NOME*/}
+{/*NOME  */}
         <Text style={{ color: '#0000FF', fontSize: 30, fontWeight: 'bold', textAlign: 'center', textAlign:'right', }}>Caio Felipe Vieira Ferreira</Text>
 
 {/* NUMERO */}
@@ -18,7 +18,7 @@ export default function App() {
 
 {/* NUMERO DA SALA */}
         <Text style={{ backgroundColor: '#FF00FF', color: 'black', fontSize: 20, textAlign: 'center', }}>Sala 7</Text>
-
+        
 {/* CAIXA ADCIONAL(IDADE) */}
         <Text style={{ borderWidth: 3, fontSize: 20, borderColor: '#00FF00', backgroundColor: '#4B0082', textAlign: 'center', color: 'white'  }}>Idade: 16 </Text>
       </View>
