274a9c45ef7e2655d2c7abf10a1e0620f9083beb
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2024-02-21 13:12:04 -0300

    atividade React Native Text

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ backgroundColor: 'gray', width: '100%', height: 90 }}>
+
+      </View>
+      <View style={styles.secondaryContainer}>
+
+        <Text style={{ color: '#0000FF', fontSize: 30, fontWeight: 'bold', textAlign: 'center', textAlign:'right', }}>Caio Felipe Vieira Ferreira</Text>
+
+        <Text style={{color: 'black', fontSize: 25, fontStyle: 'italic', textAlign: 'flex-start', alignItems: 'center', borderWidth: 1, borderColor: 'red', borderRadius: 25,}}>07</Text>
+
+        <Text style={{ backgroundColor: '#FF00FF', color: 'black', fontSize: 20, textAlign: 'center', }}>Sala 7</Text>
+
+        <Text style={{ borderWidth: 3, fontSize: 20, borderColor: '#00FF00', backgroundColor: '#4B0082', textAlign: 'center', color: 'white'  }}>Idade: 16 </Text>
+      </View>
+
+      <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+      </View>
     </View>
   );
 }
@@ -13,8 +28,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  secondaryContainer: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+
+  },
+  
 });
